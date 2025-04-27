1d84feca8cbe7af0b2ed277ef2e68af6c2c28972
apollo22-afk <83415221+apollo22-afk@users.noreply.github.com>
2025-04-26 21:54:56 -0300

    Update livraria.java

--- Livraria/src/structure/livraria.java
+++ Livraria/src/structure/livraria.java
@@ -1,5 +1,35 @@
 package structure;
 
-public class livraria {
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Livro;
+
+public class livraria {
+	
+	private List<Livro> livros;
+	
+	public void Livraria() {
+		livros = new ArrayList<livros>();
+	}
+
+	public void adicionarLivro(Livro livro) {
+		livros.add(livro);
+	}
+	
+	public void listarLivros() {
+		System.out.println("Livros Disponíveis");
+		for (Livro l : livros) {
+			System.out.println(l.toString())
+		}
+	}
+	
+	public Livro buscaLivroPorTitulo(String titulo) {
+		for (Livro l: livros) {
+			if (l.getTitulo().equalsIgnoreCase(titulo)) {
+				return l;
+			}
+		}
+		return null;
+	}
 }
